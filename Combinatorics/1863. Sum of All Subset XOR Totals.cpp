3440e2b167259fee6439066faef8cfe04a7c18fd
EMTIAZ UDDIN AHMED <56144461+EMTIAZ-RUET@users.noreply.github.com>
2022-03-31 18:02:35 +0600

    Create 1863. Sum of All Subset XOR Totals.cpp

---
+++ Combinatorics/1863. Sum of All Subset XOR Totals.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int subsetXORSum(vector<int>& nums) {
+        
+       int n=nums.size();
+        int ans=0;
+        
+        for(int i=0; i<(1<<n); i++)
+        {
+            int cur=0;
+            for(int j=0; j<n; j++)
+            {
+               if(i&(1<<j))
+                   cur=cur^nums[j];
+            }
+            
+            ans=ans+cur;
+        }
+        
+        return ans;
+        
+        
+        
+    }
+};
